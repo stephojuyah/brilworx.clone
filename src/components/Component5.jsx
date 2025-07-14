68422ecc80f6777a2dbf27b0d6471f1c83f4e596
stephojuyah <stephojuyah2@gmail.com>
2025-07-14 10:23:56 +0100

    added more media query functionalities

--- src/components/Component5.jsx
+++ src/components/Component5.jsx
@@ -5,30 +5,30 @@ const Component5 = () => {
 
     return(
         <>
-        <div className="contain2011">
-            <div className="logoo201">
-                <img src={logo1} alt="logo" className="img201"/>
+        <div className="contain0">
+            <div className="logo0">
+                <img src={logo1} alt="logo" className="img0"/>
             </div>
-            <div className="asidee201">
-                <div className="proces">WHATEVER YOU NEED</div>
-                <p className="bigg201">SERVICES</p>
-                <p className="smalll201">We want to make your company stand out, because first impressions count.</p>
-                <div className="childd201">
-                    <div className="babyy201">
-                        <p className="bigg202">WEB DESIGN</p>
-                        <p className="smalll202">Always look fresh with sharp designs and stunning layouts.</p>
+            <div className="aside0">
+                <div className="proces0">WHATEVER YOU NEED</div>
+                <p className="big0 big01">SERVICES</p>
+                <p className="small0 small01">We want to make your company stand out, because first impressions count.</p>
+                <div className="child0">
+                    <div className="baby0">
+                        <p className="big0">WEB DESIGN</p>
+                        <p className="small0">Always look fresh with sharp designs and stunning layouts.</p>
                     </div>
-                    <div className="babyy201">
-                        <p className="bigg202">WEB DEVELOPMENT</p>
-                        <p className="smalll202">Flexible, secure and innovative back end development that makes your site tick.</p>
+                    <div className="baby0">
+                        <p className="big0">WEB DEVELOPMENT</p>
+                        <p className="small0">Flexible, secure and innovative back end development that makes your site tick.</p>
                     </div>
-                    <div className="babyy201">
-                        <p className="bigg202">DOMAIN REGISTRATION</p>
-                        <p className="smalll202">Find the right domain to fit your company</p>
+                    <div className="baby0">
+                        <p className="big0">DOMAIN REGISTRATION</p>
+                        <p className="small0">Find the right domain to fit your company</p>
                     </div>
-                    <div className="babyy201">
-                        <p className="bigg202">HOSTING</p>
-                        <p className="smalll202">We’ll set up a hosting solution for you to give you peace of mind.</p>
+                    <div className="baby0">
+                        <p className="big0">HOSTING</p>
+                        <p className="small0">We’ll set up a hosting solution for you to give you peace of mind.</p>
                     </div>
                 </div>
 
--- src/components/Component7.jsx
+++ src/components/Component7.jsx
@@ -5,14 +5,14 @@ const Component7 = () => {
 
     return(
         <>
-        <div className="containnn201">
-            <div className="logo201">
-                <img src={logo1} alt="logo" className="img201"/>
+        <div className="contain901">
+            <div className="logo901">
+                <img src={logo1} alt="logo" className="img901"/>
             </div>
-            <div className="asideee201">
-                <div className="proces">WHATEVER YOU NEED</div>
-                <p className="biggg201">THE HOW</p>
-                <p className="smallll201">Stop blending in with the crowd and start leaving your mark on the web.</p>
+            <div className="aside901">
+                <div className="proces901">WHATEVER YOU NEED</div>
+                <p className="big901">THE HOW</p>
+                <p className="small901">Stop blending in with the crowd and start leaving your mark on the web.</p>
                 <button className="btn601">LET'S TALK ABOUT YOUR PROJECT</button>
 
             </div>
